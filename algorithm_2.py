1e3c1de0a8de62e4f3e350fc50def9d2b45b4e49
Jeffrey <44786107+JeffreyMoroSR2@users.noreply.github.com>
2022-01-14 12:51:48 +0200

    Додаю задчу з екстремумами

--- algorithm_2.py
+++ algorithm_2.py
@@ -1,14 +1,24 @@
 #Пошук локальних екстремумів у масиві
 
-numbers_list = [1, 2, 3, 0, -1, 4, 5]
-new_arr = []
+numbers_list = [2, 2, 3, 4, 3, 3, 2, 2, 1, 1, 2, 5]
+amount = 0
+flag = '0'
+
 
 for i in range(0, len(numbers_list)):
-    if numbers_list.index(numbers_list[i]) == 0 or numbers_list.index(numbers_list[i]) == len(numbers_list) - 1:
-        new_arr.append(numbers_list[i])
-    elif numbers_list[i] > numbers_list[i - 1] and numbers_list[i] > numbers_list[i + 1]:
-        new_arr.append(numbers_list[i])
-    elif numbers_list[i] < numbers_list[i - 1] and numbers_list[i] < numbers_list[i + 1]:
-        new_arr.append(numbers_list[i])
+    if i == 0 or numbers_list.index(numbers_list[i]) == len(numbers_list) - 1:
+        amount += 1
+    elif flag == "0" and numbers_list[i] > numbers_list[i - 1]:
+        flag = '1'
+    elif flag == '0' and numbers_list[i] < numbers_list[i - 1]:
+        flag = '-1'
+    elif flag == '1' and numbers_list[i] < numbers_list[i - 1]:
+        amount += 1
+        flag = '0'
+    elif flag == '-1' and numbers_list[i] > numbers_list[i - 1]:
+        amount += 1
+        flag = '0'
 
-print(new_arr)
+print(amount)
+
+
