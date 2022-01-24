56b91be26d1eb13bb633f7ab8ae55a253c9903fe
Jeffrey <44786107+JeffreyMoroSR2@users.noreply.github.com>
2022-01-24 11:42:53 +0200

    Задача з екстремумами

--- algorithm_2.py
+++ algorithm_2.py
@@ -1,12 +1,12 @@
 #Пошук локальних екстремумів у масиві
 
-numbers_list = [2, 2, 3, 4, 3, 3, 2, 2, 1, 1, 2, 5]
+numbers_list = [2, 5, 3, 4, 3, 3, 2, 2, 1, 1, 2, 5]
 amount = 0
 flag = '0'
 
 
 for i in range(0, len(numbers_list)):
-    if i == 0 or numbers_list.index(numbers_list[i]) == len(numbers_list) - 1:
+    if i == 0 or i == len(numbers_list) - 1:
         amount += 1
     elif flag == "0" and numbers_list[i] > numbers_list[i - 1]:
         flag = '1'
@@ -14,10 +14,10 @@ for i in range(0, len(numbers_list)):
         flag = '-1'
     elif flag == '1' and numbers_list[i] < numbers_list[i - 1]:
         amount += 1
-        flag = '0'
+        flag = '-1'
     elif flag == '-1' and numbers_list[i] > numbers_list[i - 1]:
         amount += 1
-        flag = '0'
+        flag = '1'
 
 print(amount)
 
